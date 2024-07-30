248c15c07a8360dbdf494e29b2d5e7cf6a57a023
technillogue <technillogue@gmail.com>
2024-07-30 04:42:06 -0400

    simplify further

--- predict.py
+++ predict.py
@@ -1,20 +1,9 @@
 import asyncio
 import random
-import logging
 import string
-import random
-from typing import AsyncIterator, Optional
+from typing import AsyncIterator
 
 from cog import BasePredictor, Path, Input
-
-
-def handle_error(task: "asyncio.Task[None]") -> None:
-    if task.cancelled():
-        return
-    exc = task.exception()
-    if exc:
-        logging.error("caught exception", exc_info=exc)
-    print(task)
 
 
 sleepy = 1
@@ -24,30 +13,16 @@ class Predictor(BasePredictor):
     async def setup(self) -> None:
         pass
 
-    #     self.task = asyncio.create_task(self.work())
-    #     self.task.add_done_callback(handle_error)
-    #     self.batch: list[tuple[str, asyncio.Queue[str]]] = []
-
-    # async def work(self) -> None:
-    #     while 1:
-    #         if not self.batch:
-    #             await asyncio.sleep(sleepy * 5)
-    #             continue
-    #         batch, self.batch = self.batch, []
-    #         for i in range(10):
-    #             for input, output in batch:
-    #                 await output.put(f"hello {input} (#{i})")
-    #             await asyncio.sleep(sleepy)
-    #         await asyncio.sleep(sleepy)
-
     async def predict(
         self,
         name: str = Input(default=None),
         name_file: Path = Input(default=None),
         n: int = 100,
         sleep: float = 0.0001,
+        log: str = True,
     ) -> AsyncIterator[str]:
-        print("this is a log message")
+        if log:
+            print("this is a log message")
         if not (name or name_file):
             raise ValueError("one of name or name_file is required")
         actual_name = open(name_file).read() if name_file else name
